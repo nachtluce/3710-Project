419245bb594d37aa7b0fcd1e6e020ff4bbb24088
unknown <user@.(none)>
2011-08-31 17:50:53 -0600

    initial commit

---
+++ ALU/ALUmod.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:51:41 08/31/2011 
+// Design Name: 
+// Module Name:    ALUmod 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALUmod(
+    input [15:0] A,
+    input [15:0] B,
+    input [3:0] opcode,
+    output [15:0] S,
+    input [3:0] opext,
+    output [4:0] CLFZN
+    );
+
+
+endmodule
