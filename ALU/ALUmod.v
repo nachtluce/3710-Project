123491204b3d3676c31bbe02bc2fe08ed844f749
Robert Christensen <robert.christensen@gmail.com>
2011-09-07 17:08:16 -0600

    Added ADD to the ALU

--- ALU/ALUmod.v
+++ ALU/ALUmod.v
@@ -19,20 +19,59 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ALUmod(
-    input [15:0] A,
-    input [15:0] B,
-    input [3:0] opcode,
-    output [15:0] S,
-    input [3:0] opext,
-    output [4:0] CLFZN
+    input wire [15:0] A,
+    input wire [15:0] B,
+    input wire [3:0] opcode,
+    output reg [15:0] S,
+    input wire [3:0] opext,
+    output reg [4:0] CLFZN
     );
 
-//start always block and add all the different executions
 
-always@(A,B,opcode)
+//start always block and add all the different executions
+always@(A,B,opcode,opext)
 	begin
-	
-	
+	  case({opcode, opext})
+	    8'b00000101: // ADD
+		 begin
+		   S = A + B;
+			CLFZN = 0;
+			if( S == 0 ) CLFZN[1] = 1'b1;
+			else         CLFZN[1] = 1'b0;
+			CLFZN[2] = (~A[15]&~B[15]&S[15]) | (A[15]&B[15]&S[15]);
+		 end
+		 
+		 8'b0101xxxx: // ADDI
+		 begin
+		   S = A + B;
+			CLFZN = 0;
+			if( S == 0 ) CLFZN[1] = 1'b1;
+			else         CLFZN[1] = 1'b0;
+			CLFZN[2] = (~A[15]&~B[15]&S[15]) | (A[15]&B[15]&S[15]);
+		 end
+/*		 
+		 8'b00000110: // ADDU
+		 begin
+			CLFZN = 0;		 
+		   {CLFZN[4],S} = A + B;
+			if( S == 0 ) CLFZN[1] = 1'b1;
+			else         CLFZN[1] = 1'b0;
+		 end
+		 
+		 8'b0110xxxx: //ADDUI
+		 begin
+			CLFZN = 0;		 
+		   {CLFZN[4],S} = A + B;
+			if( S == 0 ) CLFZN[1] = 1'b1;
+			else         CLFZN[1] = 1'b0;		 
+		 end
+*/	
+		default:
+		begin
+			CLFZN = 0;
+			S = 0;
+		end
+	  endcase
 	
 	end
 
