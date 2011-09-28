435a22e34883492d273ff4bd1a68fc4c0b0e69bd
T. James Lewis <nachtluce@gmail.com>
2011-09-27 18:25:35 -0700

    fixed errors

--- LCD_Fib.v
+++ LCD_Fib.v
@@ -36,7 +36,7 @@ module LCD_Fib(
 	wire [15:0] tempOut;
 	
 
-	Fibonacci( clk, reset, SetA, SetB, switches, tempOut);
+	Fibonacci f( clk, reset, SetA, SetB, switches, tempOut);
 	
-	lcd_ctrl(clk, reset, tempOut, SF_D, LCD_E, LCD_RS, LCD_RW);
+	lcd_ctrl l(clk, reset, tempOut, SF_D, LCD_E, LCD_RS, LCD_RW);
 endmodule
