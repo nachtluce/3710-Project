63de6dbe3a7e39d317d700f8bc57178948379d8e
Robert Christensen <robert.christensen@gmail.com>
2011-10-30 12:36:05 -0600

    changed the names of variables to match documentation

--- program_counter.v
+++ program_counter.v
@@ -2,21 +2,21 @@
 /*
 Programer: Robert Christensen
 
-clk   - input clock for the register
-reset - reset the input to zero
-inc   - if inc is set to 1 it will increase by the value of 'in'
-din   - used to load the register with a value
-wEnb  - if set to 1 the value of din will be loaded
+Clock   - input clock for the register
+Reset - Reset the input to zero
+increment   - if increment is set to 1 it will increase by the value of 'in'
+ImmIn   - used to load the register with a value
+WriteEnable  - if set to 1 the value of ImmIn will be loaded
 
 PC    - the value of the program counter
 */
 module program_counter(
-    input clk,
-    input reset,
-    input inc,
-	 input [7:0] in,
-    input [15:0] din,
-    input wEnb,
+    input Clock,
+    input Reset,
+    input increment,
+	 input [7:0] In,
+    input [15:0] ImmIn,
+    input WriteEnable,
     output reg [15:0] PC
     );
 	 
@@ -24,19 +24,19 @@ initial begin
 	PC = 16'H0000;
 end
 
-always @(posedge clk, negedge reset)
+always @(posedge Clock, negedge Reset)
 begin
-	if(~reset)
+	if(~Reset)
 		begin
 		PC <= 16'H0000;
 		end
-	else if(wEnb == 1'b1)
+	else if(WriteEnable == 1'b1)
 		begin
-		PC <= din;
+		PC <= ImmIn;
 		end
-	else if(inc == 1'b1)
+	else if(increment == 1'b1)
 		begin
-		PC <= PC + in;
+		PC <= PC + In;
 		end
 	else
 		begin
