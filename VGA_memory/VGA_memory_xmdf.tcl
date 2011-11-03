7a66195777e28b1246b366cf10e6372ee6df7df5
Robert Christensen <robert.christensen@gmail.com>
2011-11-03 17:27:45 -0600

    Added mif file for blk mem

--- VGA_memory/VGA_memory_xmdf.tcl
+++ VGA_memory/VGA_memory_xmdf.tcl
@@ -39,8 +39,16 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path VGA_memory.a
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
 incr fcount
 
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path VGA_memory.mif
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path VGA_memory.ngc
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path VGA_memory.sym
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
 incr fcount
 
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path VGA_memory.v
