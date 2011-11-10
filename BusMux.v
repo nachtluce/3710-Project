5cb1719452c4b45ddb4da7c63b06e86bab1b21ae
Robert Christensen <robert.christensen@gmail.com>
2011-11-10 16:49:46 -0700

    changed busmux to have an additional input

--- BusMux.v
+++ BusMux.v
@@ -23,6 +23,7 @@ module BusMux(
     input [15:0] InputA,
     input [15:0] InputB,
     input [15:0] InputC,
+	 input [15:0] InputD,
     output reg [15:0] Output
     );
 	
@@ -32,7 +33,7 @@ module BusMux(
 			2'h0: Output = InputA;
 			2'h1: Output = InputB;
 			2'h2: Output = InputC;
-			2'h3: Output = InputA;
+			2'h3: Output = InputD;
 		endcase
 	end
 
