9bb49fd6f20044ee79e0c2fb8978f7a23aaf4f74
Robert Christensen <robert.christensen@gmail.com>
2011-11-15 23:15:23 -0700

    Added sequence_sum.asm, a test program to test branch, loop, and storePC instructions

--- assembler/sequence_sum.asm
+++ assembler/sequence_sum.asm
@@ -1,25 +1,31 @@
-
-TEST_PC_STORE:
-	STOREPC R15
-	LOADLBL TEST_PC_STORE, R14
-	LOADLBL TEST_SUM, R13
-	CMP R14, R15
-	BEQ
-	JUMP R13	# will excecute if true
+START:	NOP #0
+	NOP #1
+	NOP #2
 	
-	MOVI 0xAD, R0	# will store values if test failed
-	MOVIU 0xDE, R0
+TEST_PC_STORE:
+	STOREPC R15     #3
+	LOADLBL TEST_PC_STORE, R14 #4,5
+	LOADLBL TEST_SUM, R13      #6,7
+	CMP R14, R15    #8
+	BEQ             #9
+	JUMP R13	#A will excecute if true
+	
+	MOVI 0xAD, R0	#B will store values if test failed
+	MOVIU 0xDE, R0  #C
 
 TEST_SUM:
-	MOVI 100, R15	# counter used
-	XOR  R14, R14	# location to store total - zero here
+	MOVI 10, R15	#D counter used
+	XOR  R14, R14	#E location to store total - zero here
+	MOVI 0xAB, R6   #F add sentanal value
 TEST_SUM1:
-	CMPI 0, R15
-	JGE TEST_SUM2
+	XOR R6, R6      #10 Remove sentanal value
+	CMPI 0, R15     #11
+	JLT TEST_SUM2   #12
 	
-	ADD R15, R14
-	SUBI 1, R15
-	JEQ TEST_SUM1	# Will always jump to TEST_SUM1
+	ADD R15, R14    #13
+	SUBI 1, R15     #14
+	JEQ TEST_SUM1	#15 Will always jump to TEST_SUM1
 
 TEST_SUM2:
-	JLS TEST_SUM2 # will always jump?
+	NOP
+	JLS TEST_SUM2 #16 will always jump?
